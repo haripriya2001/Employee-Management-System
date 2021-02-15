bfe9c2555609c7028f30862568ce2f5f2ed909af
haripriya2001 <55545850+haripriya2001@users.noreply.github.com>
2021-02-15 21:51:40 +0530

    Update DepartmentRegulations.java

--- EmployeeManagementSystem/src/emp_project/servlet/DepartmentRegulations.java
+++ EmployeeManagementSystem/src/emp_project/servlet/DepartmentRegulations.java
@@ -38,11 +38,11 @@ public class DepartmentRegulations extends HttpServlet {
 				out.println("<style>");
 				out.println("#users{font-family:sans-serif; border-collapse:collapse; width:100%;}");
 				out.println("#users td,#customers th{border:1px solid #ddd; padding:8px}");
-			    out.println("#users tr:nth-child(even){ background-color:#f2f2f2;}");
-			    out.println("#users tr:hover{background-color:#ddd;}");
-			    out.println("#users th{ padding-top:12px; padding-bottom:12px; text-align:center; background-color:#4CAF50; color:white;}");
-			    out.println("#users td{text-align:center;}");
-			    out.println("</style></head>");
+			        out.println("#users tr:nth-child(even){ background-color:#f2f2f2;}");
+			        out.println("#users tr:hover{background-color:#ddd;}");
+			        out.println("#users th{ padding-top:12px; padding-bottom:12px; text-align:center; background-color:#4CAF50; color:white;}");
+			        out.println("#users td{text-align:center;}");
+			        out.println("</style></head>");
 				out.println("<body><center><table id=users>");
 				out.println("<tr>");
 				out.println("<th>REGULATION ID</th>");
@@ -51,7 +51,7 @@ public class DepartmentRegulations extends HttpServlet {
 				out.println("<th>CREATION DATE</th>");
 				out.println("<th>DEPARTMENT ID</th>");
 				out.println("</tr>");  
-		        PreparedStatement statement=Login_db.getPreparedStatement("select * from regulations where Department_Id="+DepartmentId);
+		                PreparedStatement statement=Login_db.getPreparedStatement("select * from regulations where Department_Id="+DepartmentId);
 				ResultSet resultset=statement.executeQuery();
 				while(resultset.next())
 				{
@@ -64,8 +64,8 @@ public class DepartmentRegulations extends HttpServlet {
 						out.println("<td>"+Regulation_Id+"</td>");
 						out.println("<td>"+Regulation_Type+"</td>");
 						out.println("<td>"+Regulation_Details+"</td>");
-				        out.println("<td>"+CreationDate+"</td>");
-				        out.println("<td>"+Department_Id+"</td>");
+				                out.println("<td>"+CreationDate+"</td>");
+				                out.println("<td>"+Department_Id+"</td>");
 						out.println("</tr>");
 				}
 				 out.println("</table>");
