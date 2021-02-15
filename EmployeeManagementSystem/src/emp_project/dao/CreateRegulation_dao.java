c5dd16225af6fd5b594312ba273756d8281ad3b7
haripriya2001 <55545850+haripriya2001@users.noreply.github.com>
2021-02-15 21:47:23 +0530

    Update CreateRegulation_dao.java

--- EmployeeManagementSystem/src/emp_project/dao/CreateRegulation_dao.java
+++ EmployeeManagementSystem/src/emp_project/dao/CreateRegulation_dao.java
@@ -10,16 +10,16 @@ public class CreateRegulation_dao {
 		
 		try
 		{
-			PreparedStatement statement=Login_db.getPreparedStatement("insert into regulations values(?,?,?,?,?)");
-            statement.setInt(1, Regulation_Id);
-            statement.setString(2, Regulation_Type);
-            statement.setString(3, Regulation_Details);
-            statement.setString(4, Creation_Date);
-            statement.setInt(5,deparment_Id);
-            int count=statement.executeUpdate();
-            System.out.println(count+" row/s affected");
-            return true;
-        }catch(Exception e)
+		    PreparedStatement statement=Login_db.getPreparedStatement("insert into regulations values(?,?,?,?,?)");
+		    statement.setInt(1, Regulation_Id);
+		    statement.setString(2, Regulation_Type);
+		    statement.setString(3, Regulation_Details);
+		    statement.setString(4, Creation_Date);
+		    statement.setInt(5,deparment_Id);
+		    int count=statement.executeUpdate();
+		    System.out.println(count+" row/s affected");
+		    return true;
+                }catch(Exception e)
 		{
 		e.printStackTrace();	
 		}
