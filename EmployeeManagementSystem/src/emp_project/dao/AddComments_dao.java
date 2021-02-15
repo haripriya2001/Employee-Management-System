6d9e86120c77dee87583a2101b80d9e5c6fa58ae
haripriya2001 <55545850+haripriya2001@users.noreply.github.com>
2021-02-15 21:44:20 +0530

    Update AddComments_dao.java

--- EmployeeManagementSystem/src/emp_project/dao/AddComments_dao.java
+++ EmployeeManagementSystem/src/emp_project/dao/AddComments_dao.java
@@ -9,16 +9,16 @@ public class AddComments_dao {
 		try
 		{
 			PreparedStatement statement=Login_db.getPreparedStatement("insert into statusreport values(?,?,?,?,?,?)");
-            statement.setInt(1, Reg_Id);
-            statement.setString(2, status);
-            statement.setInt(3,Emp_Id );
-            statement.setString(4, Comments);
-            statement.setString(5,CreationDate);
-            statement.setInt(6, Dept_Id);
-            int count=statement.executeUpdate();
-            System.out.println(count+" row/s affected");
-            return true;
-        }catch(Exception e)
+		        statement.setInt(1, Reg_Id);
+		        statement.setString(2, status);
+                        statement.setInt(3,Emp_Id );
+			statement.setString(4, Comments);
+			statement.setString(5,CreationDate);
+			statement.setInt(6, Dept_Id);
+			int count=statement.executeUpdate();
+			System.out.println(count+" row/s affected");
+			return true;
+	        }catch(Exception e)
 		{
         	e.printStackTrace();	
 		}
