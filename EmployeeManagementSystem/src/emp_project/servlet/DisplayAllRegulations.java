147fbfda948db274fce7314b3742cefd9b2b1284
haripriya2001 <55545850+haripriya2001@users.noreply.github.com>
2021-02-15 21:55:13 +0530

    Update DisplayAllRegulations.java

--- EmployeeManagementSystem/src/emp_project/servlet/DisplayAllRegulations.java
+++ EmployeeManagementSystem/src/emp_project/servlet/DisplayAllRegulations.java
@@ -29,13 +29,13 @@ public class DisplayAllRegulations extends HttpServlet
 		try{
 			out.println("<html><head><title>USERS</title><link rel='stylesheet' href='button.css'>");
 			out.println("<style>");
-		    out.println("#users{font-family:sans-serif; border-collapse:collapse; width:100%;}");
-		    out.println("#users td,#customers th{border:1px solid #ddd; padding:8px}");
-		    out.println("#users tr:nth-child(even){ background-color:#f2f2f2;}");
-		    out.println("#users tr:hover{background-color:#ddd;}");
-		    out.println("#users th{ padding-top:12px; padding-bottom:12px; text-align:center; background-color:#4CAF50; color:white;}");
-		    out.println("#users td{text-align:center;}");
-		    out.println("</style></head>");
+		        out.println("#users{font-family:sans-serif; border-collapse:collapse; width:100%;}");
+		        out.println("#users td,#customers th{border:1px solid #ddd; padding:8px}");
+		        out.println("#users tr:nth-child(even){ background-color:#f2f2f2;}");
+		        out.println("#users tr:hover{background-color:#ddd;}");
+		        out.println("#users th{ padding-top:12px; padding-bottom:12px; text-align:center; background-color:#4CAF50; color:white;}");
+		        out.println("#users td{text-align:center;}");
+		        out.println("</style></head>");
 			out.println("<body><center><table id=users>");
 			out.println("<tr>");
 			out.println("<th>REGULATION ID</th>");
@@ -44,10 +44,10 @@ public class DisplayAllRegulations extends HttpServlet
 			out.println("<th>CREATION DATE</th>");
 			out.println("<th>DEPARTMENT ID</th>"); 
 			out.println("</tr>");
-	        PreparedStatement statement=Login_db.getPreparedStatement("select * from regulations");
+	                PreparedStatement statement=Login_db.getPreparedStatement("select * from regulations");
 			ResultSet resultset=statement.executeQuery();
-		    while(resultset.next())
-		    {
+		        while(resultset.next())
+		       {
 				int Regulation_Id=resultset.getInt(1);
 				String Regulation_Type=resultset.getString(2);
 				String Regulation_Details=resultset.getString(3);
@@ -59,11 +59,11 @@ public class DisplayAllRegulations extends HttpServlet
 				out.println("<td>"+Regulation_Details+"</td>");
 				out.println("<td>"+Cretaion_Date+"</td>");
 				out.println("<td>"+Department_Id+"</td>");
-		        out.println("</tr>");
+		                out.println("</tr>");
 		    }
 		    out.println("</table>");
 		    out.println("<input type='button' value='Home' onclick=\"location.href='AdminHome.jsp'\">");
-			out.println("</center></body></html>");
+		    out.println("</center></body></html>");
 		 }catch(Exception e)
 			{
 			 e.printStackTrace();
