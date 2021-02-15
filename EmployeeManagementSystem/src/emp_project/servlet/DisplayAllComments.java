85f8318cf3c54d7a6c284bf36656e4b8d4f0dc1c
haripriya2001 <55545850+haripriya2001@users.noreply.github.com>
2021-02-15 21:52:47 +0530

    Update DisplayAllComments.java

--- EmployeeManagementSystem/src/emp_project/servlet/DisplayAllComments.java
+++ EmployeeManagementSystem/src/emp_project/servlet/DisplayAllComments.java
@@ -1,6 +1,5 @@
 package emp_project.servlet;
 import java.io.IOException;
-
 import java.io.PrintWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -29,13 +28,13 @@ public class DisplayAllComments extends HttpServlet {
 		try{
 			out.println("<html><head><title>USERS</title><link rel='stylesheet' href='button.css'>");
 			out.println("<style>");
-		    out.println("#users{font-family:sans-serif; border-collapse:collapse; width:100%;}");
-		    out.println("#users td,#customers th{border:1px solid #ddd; padding:8px}");
-		    out.println("#users tr:nth-child(even){ background-color:#f2f2f2;}");
-		    out.println("#users tr:hover{background-color:#ddd;}");
-		    out.println("#users th{ padding-top:12px; padding-bottom:12px; text-align:center; background-color:#4CAF50; color:white;}");
-		    out.println("#users td{text-align:center;}");
-		    out.println("</style></head>");
+		        out.println("#users{font-family:sans-serif; border-collapse:collapse; width:100%;}");
+		        out.println("#users td,#customers th{border:1px solid #ddd; padding:8px}");
+		        out.println("#users tr:nth-child(even){ background-color:#f2f2f2;}");
+		        out.println("#users tr:hover{background-color:#ddd;}");
+		        out.println("#users th{ padding-top:12px; padding-bottom:12px; text-align:center; background-color:#4CAF50; color:white;}");
+		        out.println("#users td{text-align:center;}");
+		        out.println("</style></head>");
 			out.println("<body><center><table id=users>");
 			out.println("<tr>");
 			out.println("<th>REGULATION ID</th>");
@@ -45,11 +44,11 @@ public class DisplayAllComments extends HttpServlet {
 			out.println("<th>CREATION DATE</th>");
 			out.println("<th>DEPARTMENT ID</th>");
 			out.println("</tr>");
-		    PreparedStatement statement=Login_db.getPreparedStatement("select * from statusreport");
+		        PreparedStatement statement=Login_db.getPreparedStatement("select * from statusreport");
 			ResultSet resultset=statement.executeQuery();
 			while(resultset.next())
 			{
-		    int Regulation_Id=resultset.getInt(1);
+		        int Regulation_Id=resultset.getInt(1);
 			String Status=resultset.getString(2);
 			int Emp_Id=resultset.getInt(3);
 			String Comments=resultset.getString(4);
@@ -61,7 +60,7 @@ public class DisplayAllComments extends HttpServlet {
 				out.println("<td>"+Emp_Id+"</td>");
 				out.println("<td>"+Comments+"</td>");
 				out.println("<td>"+CreationDate+"</td>");
-		        out.println("<td>"+Department_Id+"</td>");
+		                out.println("<td>"+Department_Id+"</td>");
 				out.println("</tr>");
 		    }
 		    out.println("</table>");
